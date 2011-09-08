59616162efb4d6b1d66bc9ce7d13129660cf6d11
Mathias Bynens <mathias@qiwi.be>
2011-09-08 22:48:34 +0200

    Show a warning and ask for user confirmation before running the bootstrapper. Fixes #8.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,6 @@
-git pull && rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+git pull
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+fi
+echo
