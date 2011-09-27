61a11c914eaa568b313b6528ab592859904e62e7
Matijs Brinkhuis <matijs@gmail.com>
2011-09-27 22:58:44 +0300

    make it possible for bootstap.sh to be executed from wherever

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
+currentdir=`pwd`
+dotdir=`dirname $0`
+cd $dotdir
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
@@ -5,3 +8,4 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 fi
 source "$HOME/.bash_profile"
+cd $currentdir
