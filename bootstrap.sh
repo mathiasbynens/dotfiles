37301ad41aa1d3496e56f57d76b01d5e5297cda9
Matijs Brinkhuis <matijs@gmail.com>
2011-09-28 15:29:37 +0300

    Undid some silly mistakes and made the script more simplerder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
-currentdir=`pwd`
-dotdir=`dirname $0`
-cd $dotdir
+#!/bin/bash
+cd "$(dirname "$0")";
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
@@ -8,4 +7,3 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 fi
 source "$HOME/.bash_profile"
-cd $currentdir
