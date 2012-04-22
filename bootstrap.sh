6288b5ba850a868fccf60e27808248106646f79c
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-04-22 20:55:06 +0200

    Improve the .extra file handling

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,12 @@ cd "$(dirname "$0")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	if [ -f ~/.extra ]; then
+		echo "These changes have been made in the .extra.dist file, please handle these manually"
+    	diff .extra.dist ~/.extra
+	else
+	    mv ~/.extra.dist ~/.extra
+	fi
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
