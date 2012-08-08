0b9c16e4184c539be06ddb09156fce96ce2cd3d4
Shane Vitarana <shanev@gmail.com>
2012-08-08 00:31:31 -0700

    made .brew and .osx explicit shell scripts instead of dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "brew.sh" --exclude "osx.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
