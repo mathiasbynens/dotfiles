aed63f912f6a270e19d3e4184bda1b98e6f9ecbb
Bruno Germano <bruno.germano@gmail.com>
2012-08-31 16:46:32 -0300

    remove git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,25 +25,4 @@ else
 fi
 unset doIt
 
-# git config
-
-#make "ci" alias for "commit"
-git config --global alias.ci commit
-#make "co" alias for checkout
-git config --global alias.co checkout
-#make "br" alias for branch
-git config --global alias.br branch
-#make "st" alias for status
-git config --global alias.st status
-
-# config name and mail
-git config --global user.name "Bruno Germano"
-git config --global user.email bruno.germano@gmail.com
-
-# colorize git
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-
 source ~/.bash_profile
