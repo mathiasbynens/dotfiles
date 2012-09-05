82993f731c9c40c39d1e607ed7efad03b5bca216
Bruno Germano <bruno.germano@gmail.com>
2012-09-05 21:41:13 +0000

    update brew instalations packages

--- brew.sh
+++ brew.sh
@@ -17,32 +17,20 @@ brew install bash
 # Install wget with IRI support
 brew install wget --enable-iri
 
-# Install RingoJS and Narwhal
-# Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
-# brew install ringojs
-# brew install narwhal
-
 # Install more recent versions of some OS X tools
-brew tap homebrew/dupes
+brew tap     homebrew/dupes
 brew install homebrew/dupes/grep
-brew tap josegonzalez/homebrew-php
+brew tap     josegonzalez/homebrew-php
 brew install php54
 
-# These two formulae didn’t work well last time I tried them:
-#brew install homebrew/dupes/vim
-#brew install homebrew/dupes/screen
+# databases
+brew install mysql
+brew install mongodb
 
 # Install everything else
-# brew install ack
-#brew install exiv2
 brew install git
-#brew install imagemagick
-# brew install lynx
+brew install graphicmagick
 brew install node
-# brew install rename
-# brew install rhino
-# brew install tree
-# brew install webkit2png
 
 # install git and bash-completion 
 brew install bash-completion git
