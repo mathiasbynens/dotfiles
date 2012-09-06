c7518b107d195c9ab838581daaa9d0263050fb3d
Bruno Germano <bruno.germano@gmail.com>
2012-09-06 00:59:12 +0000

    npm installs for me dev enviroment

---
+++ npm.sh
@@ -0,0 +1,12 @@
+
+export PATH=/usr/local/share/npm/bin:$PATH
+
+npm update
+
+# install tools
+npm install less -g
+npm install stylus -g
+npm install stylus-spriting -g
+
+#h5bp build script
+ npm install https://github.com/h5bp/node-build-script/tarball/master -g
