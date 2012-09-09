7f8b381b9343c90a114a66ba65889e5f99154a8f
Paul Irish <paul.irish@gmail.com>
2012-09-08 18:41:30 -0700

    new file : install-deps. Setup of environmenty things I need.

---
+++ install-deps.sh
@@ -0,0 +1,41 @@
+# up to you (me) if you want to run this as a file or copy paste at your leisure
+
+
+# https://github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+# https://rvm.io
+# rvm for the rubiess
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# https://github.com/isaacs/nave
+# needs npm, obviously.
+# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
+npm install -g nave
+
+
+# homebrew!
+# you need the code CLI tools YOU FOOL. (TODO: i wanna try kennethreitz's gcc osx installer isntead..)
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+
+
+# https://github.com/rupa/z
+# z, oh how i love you
+mkdir -p ~/code/z
+curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
+chmod +x ~/code/z/z.sh
+
+# add this to the bash_profile file if it aint there.
+#   . ~/code/z/z.sh
+
+
+
+# my magic photobooth symlink -> dropbox. I love it.
+# first move Photo Booth folder out of Pictures and into dropbox/public
+# then:
+ln -s ~/Dropbox/Public/Photo\ Booth/ ~/Pictures/Photo\ Booth
+# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
+# then you grab public URL and send off your video message in a heartbeat.
+
