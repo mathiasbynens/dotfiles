e27e34b858411af14e3993a028430636dc88ca5f
Paul Irish <paul.irish@gmail.com>
2012-09-22 14:13:39 -0700

    hosts alias for editing hosts quickly.

--- install-deps.sh
+++ install-deps.sh
@@ -26,6 +26,8 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 mkdir -p ~/code/z
 curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
 chmod +x ~/code/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
+
 
 # add this to the bash_profile file if it aint there.
 #   . ~/code/z/z.sh
