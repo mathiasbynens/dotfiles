e5181e5ebbc042a0ff35c11b51a3808ccb02bb18
Paul Irish <paul.irish@gmail.com>
2012-10-13 19:33:15 -0700

    docs on photobooth hack. pygments install.

--- install-deps.sh
+++ install-deps.sh
@@ -39,13 +39,16 @@ git clone git://github.com/dronir/SpotifyControl.git
 
 
 # my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures and into dropbox/public
-# then:
-ln -s ~/Dropbox/Public/Photo\ Booth/ ~/Pictures/Photo\ Booth
+# first move Photo Booth folder out of Pictures
+# then start Photo Booth. It'll ask where to put the library.
+# put it in Dropbox/public
+
 # now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
 # then you grab public URL and send off your video message in a heartbeat.
 
 
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
 
 
 # chrome canary as default
