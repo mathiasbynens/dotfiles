7cc57b82c713fa66a2dab7b5affbe21e079b7509
xol <xol@github.com>
2013-02-07 01:29:23 +0100

    bootstrap.sh à la spf13: uses symlinks instead of rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
+
+echo "Self update"
 git pull
+
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+    dirss=`find . -type d \( ! -name ".git*" ! -path "*/.git/*" \) | cut -c 3-`
+
+    files=`find . -type f \( ! -path "*/.git/*" \
+                             ! -name "README.md" \
+                             ! -name ".DS_Store" \
+                             ! -name "bootstrap.sh" \) | cut -c 3-`
+
+    echo "Create directory hierarchy"
+    for d in $dirss
+    do
+        mkdir -p "$HOME/$d"
+    done
+
+    echo "Setting up symlinks"
+    for f in $files
+    do
+        ln -snf "$PWD/$f" "$HOME/$f"
+    done
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 unset doIt
 source ~/.bash_profile
