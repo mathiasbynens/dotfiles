450e2b6b9be5fb299874bba529b6a54ffeb5265d
Thanasis Polychronakis <thanpolas@gmail.com>
2013-07-15 14:40:33 +0300

    tweak dotfiles

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "temp" --exclude "README.md" --exclude "sync.sh" -av . ~
+	rsync --exclude"install-deps.sh" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "temp" --exclude "README.md" --exclude "sync.sh" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
