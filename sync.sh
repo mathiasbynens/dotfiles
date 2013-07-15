3ed8268946935f0bc0fc82b2eade094920ccd90c
thanpolas <thanpolas@gmail.com>
2013-07-15 13:24:24 +0300

    air fixes

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "temp" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "temp" --exclude "README.md" --exclude "sync.sh" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
