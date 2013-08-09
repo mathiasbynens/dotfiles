2875ad0f49c6ff782b8167b6aed91240194ac164
Bruno Germano <bruno.germano@gmail.com>
2013-08-09 21:07:55 +0000

    task to use a configuration in dorpbox folder

---
+++ sublime.sh
@@ -0,0 +1,13 @@
+# Sublime config folders
+
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Backup
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Settings
+
+ln -s ~/Dropbox/Aplicativos/Sublime\ Text\ 2/Backup ~/Library/Application\ Support/Sublime\ Text\ 2/Backup
+ln -s ~/Dropbox/Aplicativos/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages
+ln -s ~/Dropbox/Aplicativos/Sublime\ Text\ 2/Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+ln -s ~/Dropbox/Aplicativos/Sublime\ Text\ 2/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages
+ln -s ~/Dropbox/Aplicativos/Sublime\ Text\ 2/Settings ~/Library/Application\ Support/Sublime\ Text\ 2/Settings
