2d833272fe45fdc0ca4b5ba38d70eb4ce3966a6e
Bruno Germano <bruno.germano@gmail.com>
2013-08-09 21:07:30 +0000

    update npm tools instalation

--- npm.sh
+++ npm.sh
@@ -2,9 +2,11 @@
 npm update
 
 # install tools
-npm install less -g
+npm install grunt-cli -g
+npm install yo -g
+npm install generator-webapp -g
 npm install stylus -g
 npm install stylus-spriting -g
 
 #h5bp build script
- npm install https://github.com/h5bp/node-build-script/tarball/master -g
+ # npm install https://github.com/h5bp/node-build-script/tarball/master -g
