a5ac20561c44e3b81e9031aa783e403b59567e68
Randy Pond <randito@gmail.com>
2014-02-15 12:26:19 -0600

    Don't rsync the vim swap and backup directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude ".vim/backups" --exclude ".vim/swaps" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bash_profile
 }
