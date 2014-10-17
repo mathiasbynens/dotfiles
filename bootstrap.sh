f41a601e8b83161e6f711ebd4f26bb832310aa72
Paul Melnikow <email@paulmelnikow.com>
2014-10-17 07:58:47 -0400

    bootstrap.sh: List exclude patterns in a file
    To make this more copy-paste friendly, this also makes the patterns a little more flexible and excludes vi temporary files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	rsync --exclude-from .IGNORE -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
