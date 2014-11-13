e55f57d7ff27e8e0908190c38ae6d3880852aa59
Daniel Craig Jallits <dan@jallits.com>
2014-11-13 10:01:13 -0600

    Update gnu-sed Option
    Removed the following warning:
    `Warning: gnu-sed: --default-names was deprecated; using --with-default-names instead!`

--- brew.sh
+++ brew.sh
@@ -24,7 +24,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --default-names
+brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
