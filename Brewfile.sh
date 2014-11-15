19c4df1366276bc895641c5052ad12b87a534252
Maksim Pecherskiy <max@maksimize.com>
2014-11-15 09:24:15 -0800

    Minor tweaks plus brew shell script.

---
+++ Brewfile.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+# Install command-line tools using Homebrew
+# Usage: `brew bundle Brewfile`
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+#sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --default-names
+# Install Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+brew install bash-completion
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install RingoJS and Narwhal
+# Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
+brew install ringojs
+brew install narwhal
+
+# Install more recent versions of some OS X tools
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install nmap
+brew install pngcheck
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` et al.
+brew install xpdf
+brew install xz
+
+# Install other useful binaries
+brew install ack
+#install exiv2
+brew install git
+brew install imagemagick --with-webp
+install node # This installs `npm` too using the recommended installation method
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rhino
+brew install tree
+brew install webkit2png
+brew install zopfli
+
+brew install homebrew/versions/lua52
+
+# Remove outdated versions from the cellar
+brew cleanup
