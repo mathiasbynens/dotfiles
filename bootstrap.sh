527561f759eb4409ba52d90a0676a7cb6ee4d720
Guixing Bai <khsing.cn@gmail.com>
2014-11-28 21:41:25 +0800

    reduce dependence to init folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "init" --exclude "brew.sh" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
