4de6b727cc4f80eff88ec5af35e721cdf95724fe
Mathias Bynens <mathias@qiwi.be>
2015-01-06 23:25:01 +0100

    brew.sh: Install `openssh`
    This includes a more up-to-date `ssh` binary.

--- brew.sh
+++ brew.sh
@@ -43,6 +43,7 @@ brew install narwhal
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 brew install homebrew/php/php55 --with-gmp
 
