3cfc9bc98d4f74a72a1764e61450e8f15b108fb8
Tom Fuertes <tomfuertes@gmail.com>
2015-03-06 12:55:42 -0600

    cd back after dup

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+  cd -
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
