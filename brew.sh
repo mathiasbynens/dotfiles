ea959c5706472c939ac6d0c01df8d1b1f58241b0
Mathias Bynens <mathias@qiwi.be>
2015-03-25 16:19:05 +0100

    brew.sh: Add aircrack-ng

--- brew.sh
+++ brew.sh
@@ -54,6 +54,7 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
 brew install bfg
 brew install binutils
 brew install binwalk
