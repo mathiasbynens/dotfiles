0a7d20800ed130f2294bae22abbd4c9f7023d640
Ravi Chamarthy <chamarty@yahoo.com>
2015-04-06 17:00:52 -0700

    Customize brew and .path

--- brew.sh
+++ brew.sh
@@ -37,8 +37,8 @@ brew install wget --with-iri
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+#brew install ringojs
+#brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
@@ -95,6 +95,9 @@ brew install ssh-copy-id
 brew install tree
 brew install webkit2png
 brew install zopfli
+
+# Install rbenv
+brew install rbenv
 
 # Remove outdated versions from the cellar.
 brew cleanup
