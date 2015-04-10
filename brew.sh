1d6664ec420ff19dae837def525f1b7af1cef8f6
Mathias Bynens <mathias@qiwi.be>
2015-04-10 18:07:39 +0200

    brew.sh: Use bash-completion2 for improved Bash 4 completion
    Hat tip: @elyscape in https://github.com/mathiasbynens/dotfiles/commit/91be10dcee350465566a48cbe303686d2d989d6d#commitcomment-10654929.

--- brew.sh
+++ brew.sh
@@ -29,7 +29,8 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew install bash-completion
+brew tap homebrew/versions
+brew install bash-completion2
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
