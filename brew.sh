f9164add291b4dee98d1db20217dab08c3369c8f
Christian Vuerings <vueringschristian@gmail.com>
2016-01-17 09:40:27 -0800

    brew.sh: Upgrade to PHP 5.6 (from 5.5)
    PHP 5.5 caused a segmentation fault when trying to run `arc`, the `anarcist` command.
    More info at http://stackoverflow.com/a/33512066/117193

--- brew.sh
+++ brew.sh
@@ -46,7 +46,7 @@ brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
