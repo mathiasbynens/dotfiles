8ee75e9be12c3d5d4e6e39ea63cac16e6894f19a
Jeffrey <jsamarziya@users.noreply.github.com>
2016-05-26 23:21:11 -0700

    simplified bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
-
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+      --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
