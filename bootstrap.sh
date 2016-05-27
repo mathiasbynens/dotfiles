183ceb943fefb35f04e14fe15e2e7915a32985c6
Jeffrey <jsamarziya@users.noreply.github.com>
2016-05-26 23:53:55 -0700

    reformatted excluded .osx from rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,10 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-      --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
+rsync --exclude ".git/" \
+      --exclude ".DS_Store" \
+      --exclude "bootstrap.sh" \
+      --exclude "README.md" \
+      --exclude "LICENSE-MIT.txt" \
+      --exclude ".osx" \
+      -avh --no-perms . ~
