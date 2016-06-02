5c15c5ecdee0b9288f7a39d2ca8eb0ece16c47f4
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-06-01 22:53:46 -0700

    added conditional exclude of .minttyrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+opts='-avh --no-perms'
+opts+=' --exclude .git/'
+opts+=' --exclude .DS_Store'
+opts+=' --exclude bootstrap.sh'
+opts+=' --exclude README.md'
+opts+=' --exclude LICENSE-MIT.txt'
+opts+=' --exclude .osx'
+
+if [ "$OSTYPE" != "cygwin" ]; then
+    opts+=' --exclude .minttyrc'
+fi 
+
 cd "$(dirname "${BASH_SOURCE}")"
 
-rsync --exclude ".git/" \
-      --exclude ".DS_Store" \
-      --exclude "bootstrap.sh" \
-      --exclude "README.md" \
-      --exclude "LICENSE-MIT.txt" \
-      --exclude ".osx" \
-      -avh --no-perms . ~
+rsync $opts . ~
