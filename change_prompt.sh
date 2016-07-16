2bc4f1c1b27942d3a74970512faf23be87da04de
main <main@mains-MacBook-Air.local>
2016-07-16 13:52:42 -0500

    update name of file

--- change_prompt.sh
+++ change_prompt.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# This script swaps one prompt style for another
+
 solarized_prompt=$(ls .solarized_prompt 2> /dev/null | wc -l)
-smyck_prompt=$(ls .smyck_prompt 2> /dev/null | wc-l)
+smyck_prompt=$(ls .smyck_prompt 2> /dev/null | wc -l)
 
 if [ $solarized_prompt = 0 ] && [ $smyck_prompt = 1 ]; then
         mv ~/.bash_prompt ~/.solarized_prompt
@@ -12,3 +14,6 @@ elif [ $solarized_prompt = 1 ] && [ $smyck_prompt = 0 ]; then
 else
         echo You are missing at least one prompt file.
         echo Run ~/dotfiles/bootstrap.sh
+fi
+
+exec /usr/bin/env/ bash
