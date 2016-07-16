76e65e6660802f4670d8b3a6efbb0a55d80bd399
Kevin O'Connor <oconn.kv@gmail.com>
2016-07-16 15:35:40 -0500

    update build_extra script

--- build_extra.sh
+++ build_extra.sh
@@ -4,13 +4,12 @@
 # The .extra file will hold tmux & possibly other redentials
 
 echo "Hi,"
-echo "\tThis program will promopt you for some information, and use your responses to build some configurations for you."
+echo "    This program will promopt you for some information, and use your responses to build some configurations for you."
 echo
 echo "Please enter a tmux alias:"
 read tmux_alias
 echo
 echo "Please enter a default tmux session name"
-echo
 read tmux_session
 
 rm ~/.extra 2> /dev/null
@@ -18,4 +17,6 @@ rm ~/.extra 2> /dev/null
 cat << EOT >> ~/.extra
 # This file contains additional configurations
 alias $tmux_alias="tmux a -t $tmux_session || tmux new -s $tmux_session"
+EOT
 
+source ~/.extra
