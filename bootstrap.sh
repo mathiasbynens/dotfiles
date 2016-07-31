48ed5172332a9d90ecfcdf5721acec3a28b364dd
Janos Gyerik <janos@kronos>
2016-07-31 15:15:19 +0200

    Remove redundant quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo ""
+	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
