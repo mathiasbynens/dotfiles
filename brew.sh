6bbc347d03a6e4e6101c1cc6cf25eeacd7999e76
R4YM3 <raymond@hetwebbureau.nl>
2017-04-12 22:07:07 +0200

    removed no using

--- brew.sh
+++ brew.sh
@@ -15,33 +15,15 @@ sudo spctl --master-disable
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 
-# # Install some other useful utilities like `sponge`.
-# brew install moreutils
-# # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-# brew install findutils
-# # Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed --with-default-names
-# # Install Bash 4.
-# # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# # running `chsh`.
-# brew install bash
-# brew tap homebrew/versions
-# brew install bash-completion2
-
-# # Switch to using brew-installed bash as default shell
-# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-#   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-#   chsh -s /usr/local/bin/bash;
-# fi;
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
 
 # # Install `wget` with IRI support.
-# brew install wget --with-iri
-
-# # Install RingoJS and Narwhal.
-# # Note that the order in which these are installed is important;
-# # see http://git.io/brew-narwhal-ringo.
-# brew install ringojs
-# brew install narwhal
+brew install wget --with-iri
 
 # # Install more recent versions of some macOS tools.
 # brew install vim --with-override-system-vi
@@ -50,64 +32,10 @@ brew install coreutils
 # brew install homebrew/dupes/screen
 # brew install homebrew/php/php56 --with-gmp
 
-# # Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-
-# # Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
-# # Install other useful binaries.
-# brew install ack
-# brew install dark-mode
-# #brew install exiv2
-# brew install git
-# brew install git-lfs
-# brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rhino
-# brew install speedtest_cli
-# brew install ssh-copy-id
-# brew install testssl
-# brew install tree
-# brew install vbindiff
-# brew install webkit2png
-# brew install zopfli
+# Install other useful binaries.
 
 # programs
-brew install mas
-brew install wget
-brew install lynx
+brew install dark-mode
 brew install git
 brew install git-flow
 brew install gpg
@@ -115,16 +43,23 @@ brew install grc
 brew install gls
 brew install heroku-toolbelt
 heroku update
+brew install lynx
+brew install mas
 brew install pyenv
 brew install pyenv-virtualenv
 brew install pyenv-virtualenvwrapper
 brew install nodeenv
 brew install mysql
 brew install redis
+brew install rename
+brew install testssl
+brew install tree
 brew install node
 brew install mongodb --with-openssl
 sudo mkdir -p /data/db
 sudo chown -R `id -u` /data/db
+brew install webkit2png
+brew install wget
 
 npm install -g bower
 npm install -g nodemon
