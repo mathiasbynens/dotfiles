5bc1d252ee451488996ab7d1807e2cf3b7be896a
R4YM3 <raymond@hetwebbureau.nl>
2017-04-24 13:22:53 +0200

    Added sublime settings

--- .sublime/setup.sh
+++ .sublime/setup.sh
@@ -1,4 +1,7 @@
 # create symbolic link to sublime config files
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 rm -r User
-ln -s ~/Dropbox/Sublime/User
+ln -s ~/.sublime/User
+
+echo "Start sublime text and install package control, see https://packagecontrol.io/"
+echo "When installed restart Sublime"
