f5c2ea359a0e4623450357ee7b10b5f57929c7ee
Bruno Germano <bruno.germano@gmail.com>
2018-04-02 12:09:37 -0300

    add packages install for ubuntu

---
+++ apt.sh
@@ -0,0 +1,45 @@
+# Access as root
+sudo -u
+
+apt-get update
+
+# install tools
+sudo apt-get install curl coreutils net-tools apt-transport-https ca-certificates software-properties-common -y
+
+# install enpass repository
+echo "deb http://repo.sinew.in/ stable main" >   /etc/apt/sources.list.d/enpass.list
+wget -O - https://dl.sinew.in/keys/enpass-linux.key | apt-key add -
+
+# install node repository
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+# install docker repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install nodejs -y
+apt-get install enpass -y
+apt-get install git-all -y
+
+# docker
+apt-get install docker-ce -y
+groupadd docker
+usermod -aG docker germano
+
+systemctl enable docker
+systemctl disable docker
+
+curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+docker run hello-world
+
+chown germano:germano /home/germano/.docker -R
+chmod g+rwx "/home/germano/.docker" -R
