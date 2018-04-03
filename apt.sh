3faeaaaaa85d8e81c6ba7b4dd2178ead69c6725d
Bruno Germano <bruno.germano@gmail.com>
2018-04-03 16:36:56 -0300

    add openvpn package

--- apt.sh
+++ apt.sh
@@ -4,7 +4,7 @@ sudo -u
 apt-get update
 
 # install tools
-sudo apt-get install curl coreutils net-tools apt-transport-https ca-certificates software-properties-common ubuntu-restricted-extras gnome-tweak-tool -y
+sudo apt-get install curl coreutils net-tools apt-transport-https ca-certificates software-properties-common ubuntu-restricted-extras gnome-tweak-tool network-manager-openvpn-gnome -y
 
 # install cafeine repository
 sudo add-apt-repository ppa:caffeine-developers/ppa
