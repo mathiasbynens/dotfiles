7e07d3f42141a1ace7c94ef9e287e712a8f8d89f
Daniele Galluccio <daniele.galluccio@mymedsandme.com>
2018-05-18 12:43:12 +0100

    update config files

--- brew.sh
+++ brew.sh
@@ -45,7 +45,7 @@ brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
-brew install homebrew/php/php56 --with-gmp
+# brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
