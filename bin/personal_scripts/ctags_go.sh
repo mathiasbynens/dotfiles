1cfd085f85f04804cc9a5fe47e9a87d337377fb1
Chris Hill <krisahil@gmail.com>
2018-06-20 09:21:46 -0400

    Added vim "commentary" comments plugin; misc config updates.

--- bin/personal_scripts/ctags_go.sh
+++ bin/personal_scripts/ctags_go.sh
@@ -2,4 +2,4 @@
 
 # Go to docroot and re-build ctags list.
 cd $1
-/usr/local/bin/ctags --langmap=php:.engine.inc.module.theme.install.php --php-kinds=cdfi --languages=php --recurse
+/usr/local/bin/ctags --langmap=php:.engine.inc.module.theme.install.php --php-kinds=cdfi --languages=php --recurse --sort=foldcase
