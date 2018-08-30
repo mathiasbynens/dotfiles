21a52ae1c3a14a6eeae0ad7f3006aff6bf1b7ac0
Raymond Schweers <picard@mbp-van-raymond.home>
2018-08-30 22:31:00 +0200

    typo

--- config/git/install.sh
+++ config/git/install.sh
@@ -8,15 +8,15 @@ CPATH="$(pwd)/${CDIR#./}"
 
 echo "creating symlink at ~/.gitattributes"
 rm -rf ~/.gitattributes
-ln -s $CPATH/.git ~/.gitattibutes
+ln -s $CPATH/.gitattributes ~/.gitattributes
 
 echo "creating symlink at ~/.gitconfig"
 rm -rf ~/.gitconfig
-ln -s $CPATH/.git ~/.gitconfig
+ln -s $CPATH/.gitconfig ~/.gitconfig
 
 echo "creating symlink at ~/.gitignore"
 rm -rf ~/.gitignore
-ln -s $CPATH/.git ~/.gitignore
+ln -s $CPATH/.gitignore ~/.gitignore
 
 echo ""
 echo "DONE"
