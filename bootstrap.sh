ca137543f11ecb24d3d91891d031ef72bc1085a7
Daniele Galluccio <daniele.galluccio@mymedsandme.com>
2019-01-07 16:55:10 +0000

    updated aliases and minor fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,8 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "addDockIcon.sh" \
+		--exclude "brew.sh" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
