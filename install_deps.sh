6699285d68217ddcd105c09828b385547bad0f8e
Artem Vitiuk <artem@vitiuk.me>
2019-01-23 16:10:08 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # homebrew!
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
-
-sudo easy_install pip
-sudo pip install ansible
-
-sudo pip install flake8
-sudo pip install Jinja2
-sudo pip install pep8
-sudo pip install thefuck
-sudo pip install virtualenv
