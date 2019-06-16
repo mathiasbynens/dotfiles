20231595015d92abf69495961e8fac5b5bf70163
Artem Vitiuk <artem@vitiuk.me>
2019-06-16 12:04:18 +0200

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # homebrew!
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+curl https://cht.sh/:cht.sh > ~/bin/cht.sh
+chmod +x ~/bin/cht.sh
