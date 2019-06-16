ff6608d3c9fc3aad6dce314331c0ea9ce680caa6
Artem Vitiuk <artem@vitiuk.me>
2019-06-16 12:48:04 +0200

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
 # homebrew!
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# cli cheatsheet
 curl https://cht.sh/:cht.sh > ~/bin/cht.sh
 chmod +x ~/bin/cht.sh
