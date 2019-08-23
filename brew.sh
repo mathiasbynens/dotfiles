e42090bf49f860283951041709163653c8a2c522
Mathias Bynens <mathias@qiwi.be>
2019-08-23 15:09:43 +0200

    .aliases: Make `mergepdf` preserve hyperlinks

--- brew.sh
+++ brew.sh
@@ -22,7 +22,7 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
+# Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
 
@@ -83,6 +83,7 @@ brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
+brew install gs
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
