3bd8f73edcd3e87aec6897de2b1f86beda9f349f
Artem Vitiuk <artem@vitiuk.me>
2019-10-02 09:48:09 +0200

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -3,8 +3,6 @@
 # homebrew!
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
-
 # cli cheatsheet
 curl https://cht.sh/:cht.sh > ~/bin/cht.sh
 chmod +x ~/bin/cht.sh
