dab44f0ff09d8dc42e043d8544a9185490615004
Bruno Germano <bruno.germano@gmail.com>
2019-10-21 22:30:56 -0300

    add snaps

---
+++ snaps.sh
@@ -0,0 +1,36 @@
+# Access as root
+sudo -u
+
+sudo snap install brave
+sudo snap install darktable
+sudo snap install gimp
+sudo snap install slack --classic
+sudo snap install spotify
+sudo snap install telegram-desktop
+sudo snap install discord
+sudo snap install htop
+sudo snap install mqtt-explorer
+sudo snap install okular
+sudo snap install postman
+sudo snap install skype --classic
+sudo snap install gnome-clocks
+
+# theme
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/home:Horst3180.list"
+wget -nv https://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo apt-get install arc-theme
+
+# flat remix gtk blue dark
+# https://www.gnome-look.org/p/1214931/
+# Icons tela black dark
+# https://www.gnome-look.org/p/1279924/
+
+# extensions
+# Emoji Selector
+# Panel World Clock lite
+# Ubuntu appindicators
+# Ubuntu dock
+# Unite
+# User themes
