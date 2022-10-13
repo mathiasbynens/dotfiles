c42d49fed0179b880efccf3cb5c09df8b81a0a03
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2022-10-13 23:04:41 +0200

    Create .functions.zsh

---
+++ .functions.zsh
@@ -0,0 +1,12 @@
+# Create a new directory and enter it
+function mkd() {
+	mkdir -p "$@" && cd "$_";
+}
+
+# Use Git’s colored diff when available
+hash git &>/dev/null;
+if [ $? -eq 0 ]; then
+	function diff() {
+		git diff --no-index --color-words "$@";
+	}
+fi;
