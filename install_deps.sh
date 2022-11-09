53f2d01ea023fe1a2331e192d7073538473b31b1
Artem Vitiuk <artem@vitiuk.me>
2022-11-09 20:12:37 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,7 @@
 # homebrew!
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+/bin/bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # cli cheatsheet
 curl https://cht.sh/:cht.sh > ~/bin/cht.sh
