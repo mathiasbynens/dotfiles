8238848fbdae51e35649b0efac6a98338a34e601
Bruno Germano <bruno.germano@gmail.com>
2024-02-07 12:01:32 -0300

    update snaps

--- snaps.sh
+++ snaps.sh
@@ -16,18 +16,6 @@ sudo snap install gnome-clocks
 sudo snap install gitkraken
 sudo snap install vscode
 
-# theme
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/home:Horst3180.list"
-wget -nv https://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key -O Release.key
-sudo apt-key add - < Release.key
-sudo apt-get update
-sudo apt-get install arc-theme
-
-# flat remix gtk blue dark
-# https://www.gnome-look.org/p/1214931/
-# Icons tela black dark
-# https://www.gnome-look.org/p/1279924/
-
 # extensions
 # Emoji Selector
 # Panel World Clock lite
